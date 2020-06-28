120c3a1f4db5c773ba5933b0e8a4c77f05e8ea20
marc <mfmunic@gmail.com>
2020-06-28 09:26:57 -0700

    Updates

---
+++ precache-manifest.3594d81925a625409745ea49b5cf0c52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffbc9223a7343758c0ab628cfffc7a4",
+    "url": "/ponggame/index.html"
+  },
+  {
+    "revision": "32e86f39b1786f50095a",
+    "url": "/ponggame/static/css/main.77eec57a.chunk.css"
+  },
+  {
+    "revision": "5cb326d792c5e5b23b7a",
+    "url": "/ponggame/static/js/2.39cfe338.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ponggame/static/js/2.39cfe338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e86f39b1786f50095a",
+    "url": "/ponggame/static/js/main.a14c7b84.chunk.js"
+  },
+  {
+    "revision": "9bfcfa8adc741f40d006",
+    "url": "/ponggame/static/js/runtime-main.142d7881.js"
+  },
+  {
+    "revision": "09c537af05aa13c526edd6f575ba70a1",
+    "url": "/ponggame/static/media/ball-incoming.09c537af.mp3"
+  },
+  {
+    "revision": "177649cab3350934ff5ebb558e8dfe35",
+    "url": "/ponggame/static/media/ball-outgoing.177649ca.mp3"
+  },
+  {
+    "revision": "f945164ca74fa34ca11838eae0995494",
+    "url": "/ponggame/static/media/game-over.f945164c.mp3"
+  }
+]);
